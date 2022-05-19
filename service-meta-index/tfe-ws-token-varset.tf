a0f8bd8add63e947c96dce9094d086f135c9fac2
andreas.marneris <andreas.marneris@kidsloop.live>
2022-05-19 14:16:14 +0100

    Re-enabling tfe specific resources after setting the tfe_token correctly

--- service-meta-index/tfe-ws-token-varset.tf
+++ service-meta-index/tfe-ws-token-varset.tf
@@ -1,8 +1,8 @@
-# resource "tfe_variable_set" "tfe-ws-token-service" {
-#   name          = "tfe-ws-token-service"
-#   description   = "This variable set contains a Terraform Cloud token for self-configuration of workspaces in the \"services-meta\" projects."
-#   organization  = local.tfe_organization
-#   workspace_ids = [
-#     local.service_meta_index_workspace_id,
-#   ]
-# }
+resource "tfe_variable_set" "tfe-ws-token-service" {
+  name          = "tfe-ws-token-service"
+  description   = "This variable set contains a Terraform Cloud token for self-configuration of workspaces in the \"services-meta\" projects."
+  organization  = local.tfe_organization
+  workspace_ids = [
+    local.service_meta_index_workspace_id,
+  ]
+}
--- service-meta-module/workspaces_meta.tf
+++ service-meta-module/workspaces_meta.tf
@@ -8,7 +8,7 @@ resource "tfe_workspace" "service-meta" {
   allow_destroy_plan = false
 
   # Deploy settings
-  # ssh_key_id         = data.tfe_ssh_key.deploy.id
+  ssh_key_id         = data.tfe_ssh_key.deploy.id
   working_directory  = local.working_directory
   vcs_repo {
     identifier   = var.service_meta_repo
@@ -17,10 +17,10 @@ resource "tfe_workspace" "service-meta" {
     oauth_token_id     = var.vcs_oauth_token_id
   }
 }
-# resource "tfe_team_access" "meta" {
-#   for_each     = var.tfe_team_access_permissions
+resource "tfe_team_access" "meta" {
+  for_each     = var.tfe_team_access_permissions
 
-#   workspace_id = tfe_workspace.service-meta.id
-#   team_id      = data.tfe_team.team_name_id_lookup[each.key].id
-#   access       = each.value
-# }
+  workspace_id = tfe_workspace.service-meta.id
+  team_id      = data.tfe_team.team_name_id_lookup[each.key].id
+  access       = each.value
+}
