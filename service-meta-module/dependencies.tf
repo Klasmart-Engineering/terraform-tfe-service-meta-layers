8c5ac2574520931748666825c7f5d3dbe17e8dfc
andreas.marneris <andreas.marneris@kidsloop.live>
2022-05-19 16:49:55 +0100

    fix: adding variable set back in

--- service-meta-module/dependencies.tf
+++ service-meta-module/dependencies.tf
@@ -1,4 +1,4 @@
-# data "tfe_ssh_key" "deploy" {
-#   name         = var.tfe_deploy_ssh_key_id
-#   organization = local.tfe_organization_name
-# }
+data "tfe_ssh_key" "deploy" {
+  name         = var.tfe_deploy_ssh_key_id
+  organization = local.tfe_organization_name
+}
