75857d635a47cf8f657bfb4992c3474f3d94972a
andreas.marneris <andreas.marneris@kidsloop.live>
2022-05-19 12:28:10 +0100

    fix: removing ssh key and rbac settings for workspace bootstrap

--- service-meta-module/workspaces_meta.tf
+++ service-meta-module/workspaces_meta.tf
@@ -17,10 +17,10 @@ resource "tfe_workspace" "service-meta" {
     oauth_token_id     = var.vcs_oauth_token_id
   }
 }
-resource "tfe_team_access" "meta" {
-  for_each     = var.tfe_team_access_permissions
+# resource "tfe_team_access" "meta" {
+#   for_each     = var.tfe_team_access_permissions
 
-  workspace_id = tfe_workspace.service-meta.id
-  team_id      = data.tfe_team.team_name_id_lookup[each.key].id
-  access       = each.value
-}
+#   workspace_id = tfe_workspace.service-meta.id
+#   team_id      = data.tfe_team.team_name_id_lookup[each.key].id
+#   access       = each.value
+# }
